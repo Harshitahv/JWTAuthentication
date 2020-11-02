17c272ee283d45df59226ad42b7cbd1edfa64a76
abc <abc@ADWEB004>
2020-11-02 15:40:01 +0530

    Add project files.

---
+++ Controllers/CallAPIController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWTClient.Controllers
+{
+    public class CallAPIController : Controller
+    {
+
+        private IConfiguration _config;
+
+        public CallAPIController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            if ((username != "Secret") || (password != "Secret"))
+                return View((object)"Wrong Username or Password");
+
+            var tokenString = GenerateJSONWebToken();
+            List<Reservation> reservationList = new List<Reservation>();
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tokenString);
+                using (var response = await httpClient.GetAsync("https://localhost:44314/Reservation")) // change API URL to yours 
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+
+            return View("Reservation", reservationList);
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            /*var claims = new[] {
+                new Claim("Name", "Bobby"),
+                new Claim(JwtRegisteredClaimNames.Email, "hello@yogihosting.com"),
+            };*/
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                expires: DateTime.Now.AddHours(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
---
+++ Controllers/jQueryApiController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWTClient.Controllers
+{
+    public class jQueryApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string Index(string username, string password)
+        {
+            if ((username != "Secret") || (password != "Secret"))
+                return "Error";
+
+            string tokenString = GenerateJSONWebToken();
+            return tokenString;
+        }
+
+        private string GenerateJSONWebToken()
+        {
+            /*var claims = new[] {
+                new Claim("Name", "Bobby"),
+                new Claim(JwtRegisteredClaimNames.Email, "hello@yogihosting.com"),
+            };*/
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MynameisJamesBond007"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: "https://www.yogihosting.com",
+                audience: "https://www.yogihosting.com",
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: credentials
+                //claims: claims
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        [HttpPost]
+        public IActionResult Reservation([FromBody]List<Reservation> rList)
+        {
+            return PartialView("Reservation", rList);
+        }
+    }
+}
